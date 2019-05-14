5a9b3c0f75190a7db635b3a68fe893e4909f280e
Élie Meignan <contact@eliepse.fr>
2019-05-14 18:16:19 +0800

    core(front-end): add tabler-ui and feather

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,12 @@
  */
 
 require('./bootstrap');
+//import 'tabler-ui/dist/assets/js/dashboard'
+const feather = require('feather-icons')
 
-window.Vue = require('vue');
+feather.replace()
+
+//window.Vue = require('vue');
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +23,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +31,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app',
-});
+//const app = new Vue({
+//    el: '#app',
+//});
