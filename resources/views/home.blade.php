f394b359661f96ede3bb295bcead32c34db8a3a7
Élie Meignan <contact@eliepse.fr>
2019-05-19 11:50:48 +0800

    change: add a disclaimer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,20 @@
 
 @section('main')
 
-    <div class="col text-center">
-        Il n'y a rien ici pour le moment.<br>
-        Revenez lorsque cet espace sera complet.
+    <div class="col-4">
+        <div class="card">
+            <div class="card-status bg-warning"></div>
+            <div class="card-header">
+                <h3 class="card-title">Avertissement</h3>
+            </div>
+            <div class="card-body">
+                <p>
+                    Cette plateforme est encore en dévelopement et n'est pas encore opérationnelle pour une utilisation
+                    porfessionnelle. De nombreuses fontionnalités n'ont pas encore été intégrées et le seront par la
+                    suite. Pour plus d'information, n'hésitez pas à contacter l'administrateur.
+                </p>
+            </div>
+        </div>
     </div>
 
 @endsection
