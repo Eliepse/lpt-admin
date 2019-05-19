7202aa14adc8f157a9e3a30c316659033644da3f
Élie Meignan <contact@eliepse.fr>
2019-05-19 16:33:24 +0800

    fix: add Days enum

---
+++ app/Enums/Days.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class Days extends Enum
+{
+    const monday = 1;
+    const tuesday = 2;
+    const wednesday = 3;
+    const thursday = 4;
+    const friday = 5;
+    const saturday = 6;
+    const sunday = 7;
+}
