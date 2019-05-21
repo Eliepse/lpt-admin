1f73bd5122c72993fb1ed04ee2fd25285b1fc45e
Élie Meignan <contact@eliepse.fr>
2019-05-21 21:59:25 +0800

    docs: add changelog

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,4 +18,53 @@
         </div>
     </div>
 
+    <div class="col-12 col-md-8 col-lg-6">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Récemment</h3>
+            </div>
+            <div class="card-body">
+                <h4>
+                    {{--<span class="tag tag-primary">0.2</span>--}}
+                    {{--<span class="text-muted small">2019-05-21</span>--}}
+                </h4>
+                <p class="text-info">Corrections suite à la version v0.1 et petits ajouts</p>
+                <h5>Ajouts</h5>
+                <ul>
+                    <li>Ajout de la possibilité de modifier les étudiants et les parents</li>
+                </ul>
+                <h5>Changements</h5>
+                <ul>
+                    <li>Les cours sont maintenant accessibles depuis la page de gestion des classes</li>
+                    <li>
+                        Les étudiants sont accessibles depuis le menu et les parents depuis la page de gestion
+                        des étudiants
+                    </li>
+                </ul>
+                <h5>Corrections</h5>
+                <ul>
+                    <li>Amélioration de certaines parties sur mobile</li>
+                    <li>Mauvais nom de colonne dans la base de données</li>
+                    <li>Affinage de la commande de création des utilisateurs</li>
+                    <li>Dépendances manquantes</li>
+                </ul>
+            </div>
+            <div class="card-body">
+                <h4>
+                    <span class="tag tag-primary">0.1</span>
+                    <span class="text-muted small">2019-05-19</span>
+                </h4>
+                <p class="text-info">Publication d'une première version du service !</p>
+                <h5>Fonctionnalités</h5>
+                <ul>
+                    <li>Ajout de classe</li>
+                    <li>Ajout de cours</li>
+                    <li>Ajout d'administrateurs et enseignants.es</li>
+                    <li>Ajout de parents et d'étudiants (enfants)</li>
+                    <li>Organisation des parents et enfants en <i>Familles.</i></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
 @endsection
