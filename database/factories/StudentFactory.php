91469171b3e5c8b68683f8b637eef1d7757368b1
Eliepse <contact@eliepse.fr>
2019-09-17 17:57:20 +0200

    fix: add family in student factory

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -2,6 +2,7 @@
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
+use App\Family;
 use App\Student;
 use Faker\Generator as Faker;
 
@@ -9,8 +10,9 @@ $factory->define(Student::class, function (Faker $faker) {
 
     return [
         'firstname' => $faker->firstName,
-        'lastname'  => $faker->firstName,
-        'birthday'  => $faker->dateTimeBetween('-12 years', 'now'),
+        'lastname' => $faker->firstName,
+        'birthday' => $faker->dateTimeBetween('-12 years', 'now'),
+        'family_id' => factory(Family::class)->create()->id,
     ];
 
 });
