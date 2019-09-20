decf54113f86fceaccecb6ef7426de6025232b42
Eliepse <contact@eliepse.fr>
2019-09-20 11:33:34 +0200

    fix: correct total income display
    fix #8

--- resources/views/models/schedule/show.blade.php
+++ resources/views/models/schedule/show.blade.php
@@ -84,7 +84,7 @@ use App\Student;
                     <tr class="small text-muted">
                         <th></th>
                         <th>
-                            {{ $schedule->getIncome() }}&nbsp;€ / {{ $schedule->getTheoricalTotalIncome() }}&nbsp;€
+                            {{ $schedule->getIncome() }}&nbsp;€ / {{ $schedule->subscriptions->isEmpty() ? $schedule->getTheoricalTotalIncome() : $schedule->getTotalIncome() }}&nbsp;€
                         </th>
                         <th>
                             <i class="fe fe-users"></i> {{ $schedule->students->count() }} / {{ $schedule->max_students }}
