563d94fc5b17c47759d6a69632fcba7af92cf635
Eliepse <contact@eliepse.fr>
2019-09-22 23:23:38 +0200

    feat: notification/alert system

--- resources/views/dashboard-master.blade.php
+++ resources/views/dashboard-master.blade.php
@@ -1,5 +1,14 @@
 @extends('root')
 
 @section('root-main')
+
+    @if(session()->has('alerts'))
+        <div class="container mt-3">
+            @foreach(session()->get('alerts') ?? [] as $alert)
+                {!! $alert->render() !!}
+            @endforeach
+        </div>
+    @endif
+
     @yield("main")
 @endsection
