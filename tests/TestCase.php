ff62f2600f6b6a1ccbaf30df40804d6db3a64a17
Eliepse <contact@eliepse.fr>
2019-09-27 03:16:23 +0200

    test: add create admin method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use App\Sets\UserRolesSet;
+use App\StaffUser;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+
+    protected function createAdmin($attributes = []): StaffUser
+    {
+        return factory(StaffUser::class)
+            ->create(array_merge(['roles' => new UserRolesSet([UserRolesSet::ADMIN])], $attributes));
+    }
+
 }
