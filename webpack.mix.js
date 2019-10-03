623fa1c689717779a00f3bfaccd3c9e75968767e
Eliepse <contact@eliepse.fr>
2019-10-03 18:17:58 +0800

    core: improved webpack compilation

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,9 +12,18 @@ const mix = require("laravel-mix");
  */
 
 mix.sass("resources/sass/app.scss", "public/css")
-    .js("resources/js/app.js", "public/js")
-    .copyDirectory("resources/images", "public/images")
-    .version();
+    .js("resources/js/app.js", "public/js");
+
+
+if (mix.inProduction()) {
+    mix.version()
+        .extract(["vue"])
+        .copyDirectory("resources/images", "public/images");
+}
+
+if (!mix.inProduction()) {
+    mix.sourceMaps();
+}
 
 mix.webpackConfig({
     stats: {
