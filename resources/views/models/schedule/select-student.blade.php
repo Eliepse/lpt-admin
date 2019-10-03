130118f8fcca7eeffc31296a063d901f4a6fe40c
Eliepse <contact@eliepse.fr>
2019-10-03 18:34:38 +0800

    feat: add search on student lists

--- resources/views/models/schedule/select-student.blade.php
+++ resources/views/models/schedule/select-student.blade.php
@@ -27,10 +27,21 @@ $days = DaysSet::getKeys();
                 <i class="fe fe-arrow-left"></i> Page de la classe</a>
         </div>
 
-        <div class="card">
+        <div class="card listjs-container">
 
             <div class="card-header">
                 <h3 class="card-title">Ajout d'un étduiant à une classe</h3>
+                <div class="d-flex">
+                    <div class="input-group input-group-sm ml-auto" style="max-width: 25rem;">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fe fe-search"></i></span>
+                        </div>
+                        <input type="text"
+                               class="form-control bg-light listjs search"
+                               placeholder="Chercher un étudiant, un parent, une famille..."
+                               data-names="studentName,parentName">
+                    </div>
+                </div>
             </div>
 
             <div class="card-table">
@@ -42,19 +53,18 @@ $days = DaysSet::getKeys();
                         <th></th>
                     </tr>
                     </thead>
-                    <tbody>
+                    <tbody class="list">
                     @foreach($students as $student)
                         <tr>
                             <td>
-                                {{ $student->getFullname(true) }}<br>
+                                <span class="studentName">{{ $student->getFullname(true) }}</span><br>
                                 <small class="text-muted">{{ $student->getAge() }} ans</small>
                             </td>
                             <td>
-                                {{
-                                    $student->parents->map(
-                                        function(\App\User $parent) {return $parent->getFullname(true); })
-                                    ->join(', ')
-                                }}
+                                        <span class="parentName">{{ $student->parents
+                                                ->map(function(\App\ClientUser $parent){
+                                                    return $parent->getFullname(true);
+                                                })->join(', ') }}</span>
                             </td>
                             <td class="text-right">
                                 <form action="{{ route('schedules.students.link', [$schedule, $student]) }}" method="POST">
--- resources/views/models/student/index.blade.php
+++ resources/views/models/student/index.blade.php
@@ -37,11 +37,24 @@ use \App\Enums\DaysEnum;
             </div>
         </div>
 
-        <div class="row my-3">
-            <div class="col">
+        <div class="row my-3 listjs-container">
+
+            <div class="col-12 mb-2">
+                <div class="input-group input-group-sm mb-1 ml-auto" style="max-width: 25rem;">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon1"><i class="fe fe-search"></i></span>
+                    </div>
+                    <input type="text"
+                           class="form-control bg-light listjs search"
+                           placeholder="Chercher un étudiant, un parent, une famille..."
+                           data-names="studentName,parentName">
+                </div>
+            </div>
+
+            <div class="col-12">
                 <div class="card">
                     <div class="card-table">
-                        <table class="table table-hover">
+                        <table class="table table-hover table-borderless">
                             <thead>
                             <tr>
                                 <th>Nom</th>
@@ -49,19 +62,18 @@ use \App\Enums\DaysEnum;
                                 <th></th>
                             </tr>
                             </thead>
-                            <tbody>
+                            <tbody class="list">
                             @foreach($students as $student)
                                 <tr>
                                     <td>
-                                        {{ $student->getFullname(true) }}<br>
+                                        <span class="studentName">{{ $student->getFullname(true) }}</span><br>
                                         <small class="text-muted">{{ $student->getAge() }} ans</small>
                                     </td>
                                     <td>
-                                        {{
-                                            $student->parents->map(
-                                                function(\App\User $parent) {return $parent->getFullname(true); })
-                                            ->join(', ')
-                                        }}
+                                        <span class="parentName">{{ $student->parents
+                                                ->map(function(\App\ClientUser $parent){
+                                                    return $parent->getFullname(true);
+                                                })->join(', ') }}</span>
                                     </td>
                                     <td class="text-right">
                                         @can('view', $student->family)
@@ -81,3 +93,10 @@ use \App\Enums\DaysEnum;
     </div>
 
 @endsection
+<script>
+    import ListJsSearch from '../../../js/components/ListJsSearch'
+
+    export default {
+        components: {ListJsSearch}
+    }
+</script>
