923c358ff16cec6bcf3927885b9e24a0498834ad
Eliepse <contact@eliepse.fr>
2019-10-04 22:36:04 +0800

    change: add today classes

--- app/Http/Controllers/Administration/HomeController.php
+++ app/Http/Controllers/Administration/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Administration;
 
 use App\Campus;
+use App\Enums\DaysEnum;
 use App\Schedule;
+use Carbon\Carbon;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Collection;
 
@@ -17,6 +19,13 @@ class HomeController extends Controller
 
     public function __invoke()
     {
-        return view('home');
+        $schedules = Schedule::query()
+            ->with(['campus:id,name', 'course:id,name', 'course.lessons:lesson_id'])
+            ->where('day', DaysEnum::getKey(Carbon::today()->dayOfWeek))
+            ->orderBy('hour')
+            ->get()
+            ->groupBy('campus_id');
+
+        return view('home', ['todaySchedules' => $schedules]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,56 @@
 
         <div class="row">
 
+            <div class="col-12 col-md-6, col-lg-4">
+                <div class="card">
+                    <div class="card-header">
+                        Cours de la journée
+                    </div>
+                    @if(!$todaySchedules->count())
+                        <div class="card-body">
+                            <p class="text-muted">Pas de classe aujourd'hui</p>
+                        </div>
+                    @else
+                        <div class="card-table" style="overflow:auto; min-height:20rem; max-height:50vh;">
+                            <table class="table table-borderless">
+                                @foreach($todaySchedules as $campusSchedules)
+                                    <thead>
+                                    <tr>
+                                        <th colspan="2" class="text-muted text-uppercase bg-light">
+                                            <small>
+                                                {{ \Illuminate\Support\Str::ucfirst($campusSchedules->first()->campus->name) }}
+                                                ({{ $campusSchedules->count() }} classes)
+                                            </small>
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($campusSchedules as $schedule)
+                                        <tr>
+                                            <td>
+                                                {{ $schedule->course->name }}<br>
+                                                <small>{{ $schedule->hour->hour }} h</small>
+                                                @if($schedule->room)
+                                                    <small>&middot; Salle {{ $schedule->room }}</small>
+                                                @endif
+                                            </td>
+                                            <td class="text-right">
+                                                @can('view', $schedule)
+                                                    <a href="{{ route('schedules.show', $schedule) }}"
+                                                       class="btn btn-icon">
+                                                        <i class="fe fe-eye"></i>
+                                                    </a>
+                                                @endcan
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                @endforeach
+                            </table>
+                        </div>
+                    @endif
+                </div>
+            </div>
 
         </div>
     </div>
